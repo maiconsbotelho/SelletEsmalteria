5bde6d0d6af14951b7ca02a6a7bae6c4f0eceb30
Maicon Botelho <maicon@maiconbotelho.com.br>
2024-02-07 22:15:52 -0300

    Criacao da Galeria

--- assets/js/script.js
+++ assets/js/script.js
@@ -48,3 +48,49 @@ function fecharMenuFora(e) {
 
 // Adiciona um ouvinte de evento de clique ao documento inteiro
 document.addEventListener('click', fecharMenuFora);
+
+
+// GALERIA
+$('.slider').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: false,
+    fade: true,
+    asNavFor: '.carousel'
+});
+
+$('.carousel').slick({
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    asNavFor: '.slider',
+    speed: 300,
+    dots: true,
+    infinite: true,
+    centerMode: true,
+    focusOnSelect: true,
+    responsive: [
+        {
+            breakpoint: 1024,
+            settings: {
+                slidesToShow: 3,
+                slidesToScroll: 1,
+                infinite: true,
+                dots: true
+            }
+        },
+        {
+            breakpoint: 600,
+            settings: {
+                slidesToShow: 2,
+                slidesToScroll: 1
+            }
+        },
+        {
+            breakpoint: 480,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+            }
+        }
+    ]
+});
